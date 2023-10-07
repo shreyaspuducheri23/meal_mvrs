10bfe0b02e5153a580cd65591d7b675b6785eb3f
Shreyas <shreyaspuducheri23@gmail.com>
2023-10-07 14:52:27 -0400

    workign phase 1 with loading screen

--- Web app/app.py
+++ Web app/app.py
@@ -1,9 +1,35 @@
 from flask import Flask, render_template, request, jsonify
+import pandas as pd
+import numpy as np
+
+def euclidean_distance(lat1, lon1, lat2, lon2):
+    # Conversion factor to convert degrees to meters: 111,139 meters per degree
+    conversion_factor = 111139  
+    
+    # Calculate differences in latitude and longitude
+    dlat = (lat2 - lat1) * conversion_factor
+    dlon = (lon2 - lon1) * conversion_factor
+    
+    # Euclidean distance formula
+    distance = np.sqrt(dlat**2 + dlon**2)
+    
+    return distance
+
+def closest_food_bank(user_lat, user_lon):
+    food_banks_df = pd.read_csv("food_bank_data_with_xy.csv", usecols=["Name", "Latitude", "Longitude"])
+    food_banks_df['distance_to_user'] = food_banks_df.apply(
+        lambda row: euclidean_distance(user_lat, user_lon, row['Latitude'], row['Longitude']), axis=1)
+
+    # Find the row of the closest food bank
+    closest_food_bank_row = food_banks_df.loc[food_banks_df['distance_to_user'].idxmin()]
+    
+    # Convert the row to a tuple
+    closest_food_bank_tuple = tuple(closest_food_bank_row)
+    
+    return closest_food_bank_tuple
 
 app = Flask(__name__)
-
-# Store geolocation data (for demonstration purposes, in a production application you'd use a database)
-geolocation_data = {}
+closest_food_bank_loc = None
 
 @app.route('/')
 def index():
@@ -14,12 +40,11 @@ def store_location():
     data = request.json
     latitude = data.get('latitude')
     longitude = data.get('longitude')
+
+    closest_food_bank_loc = closest_food_bank(latitude, longitude)
     
-    # Store the data (you can also store it in a database)
-    geolocation_data['latitude'] = latitude
-    geolocation_data['longitude'] = longitude
-    
-    return jsonify({"message": "Location stored successfully"})
+    return jsonify({"message": "closest food bank is " + closest_food_bank_loc[0]})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
